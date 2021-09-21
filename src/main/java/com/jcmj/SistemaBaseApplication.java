0bf192f06fac6dac7997fa1bf34708269f87b7be
Jose Carlos Maria Junior <jotacfs2010@hotmail.com>
2021-09-21 19:41:35 -0300

    Criando perfil de teste

--- src/main/java/com/jcmj/SistemaBaseApplication.java
+++ src/main/java/com/jcmj/SistemaBaseApplication.java
@@ -1,52 +1,13 @@
 package com.jcmj;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.jcmj.domain.Chamado;
-import com.jcmj.domain.Cliente;
-import com.jcmj.domain.Tecnico;
-import com.jcmj.domain.enus.Perfil;
-import com.jcmj.domain.enus.Prioridade;
-import com.jcmj.domain.enus.Status;
-import com.jcmj.repository.ChamadoRepository;
-import com.jcmj.repository.ClienteRepository;
-import com.jcmj.repository.TecnicoRepository;
-
 @SpringBootApplication
-public class SistemaBaseApplication implements CommandLineRunner {
-	
-	@Autowired
-	private TecnicoRepository tecnicoRepository;
-	@Autowired
-	private ClienteRepository clienteRepository;
-	@Autowired
-	private ChamadoRepository chamadoRepository;
+public class SistemaBaseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaBaseApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		
-		Tecnico tec1 = new Tecnico(null, "Sergio Barbosa", "716.417.840-24", "sergio@mail.com", "sergio@2020");
-		tec1.addPerfis(Perfil.ADMIN);
-		
-		
-		Cliente cli1 = new Cliente(null, "Linux Trovald", "559.406.800-70", "linux@mail.com", "linux@2020");
-		
-		
-		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro Chamado", tec1, cli1);
-		
-		
-		tecnicoRepository.saveAll(Arrays.asList(tec1));
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		chamadoRepository.saveAll(Arrays.asList(c1));
 	}
 
 }
---
+++ src/main/java/com/jcmj/service/DBService.java
@@ -0,0 +1,41 @@
+package com.jcmj.service;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jcmj.domain.Chamado;
+import com.jcmj.domain.Cliente;
+import com.jcmj.domain.Tecnico;
+import com.jcmj.domain.enus.Perfil;
+import com.jcmj.domain.enus.Prioridade;
+import com.jcmj.domain.enus.Status;
+import com.jcmj.repository.ChamadoRepository;
+import com.jcmj.repository.ClienteRepository;
+import com.jcmj.repository.TecnicoRepository;
+
+@Service
+public class DBService {
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private ChamadoRepository chamadoRepository;
+	
+	public void instanciaDB() {
+		
+		Tecnico tec1 = new Tecnico(null, "Sergio Barbosa", "716.417.840-24", "sergio@mail.com", "sergio@2020");
+		tec1.addPerfis(Perfil.ADMIN);	
+		
+		Cliente cli1 = new Cliente(null, "Linux Trovald", "559.406.800-70", "linux@mail.com", "linux@2020");		
+		
+		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro Chamado", tec1, cli1);		
+		
+		tecnicoRepository.saveAll(Arrays.asList(tec1));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		chamadoRepository.saveAll(Arrays.asList(c1));
+	}
+
+}
