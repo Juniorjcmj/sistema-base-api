529432577efcb62844390885f30f71bdeeaa1bc3
Jose Carlos Maria Junior <jotacfs2010@hotmail.com>
2021-09-21 18:56:39 -0300

    Anotações JPA para classes de dominio

--- src/main/java/com/jcmj/domain/Cliente.java
+++ src/main/java/com/jcmj/domain/Cliente.java
@@ -3,18 +3,27 @@ package com.jcmj.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.jcmj.domain.enus.Perfil;
+
+@Entity
 public class Cliente extends Pessoa{
 	
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Cliente() {
 		super();
-		// TODO Auto-generated constructor stub
+		addPerfis(Perfil.CLIENTE);
 	}
 
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
-		// TODO Auto-generated constructor stub
+		addPerfis(Perfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamados() {
--- src/main/java/com/jcmj/domain/Tecnico.java
+++ src/main/java/com/jcmj/domain/Tecnico.java
@@ -3,17 +3,29 @@ package com.jcmj.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.jcmj.domain.enus.Perfil;
+
+@Entity
 public class Tecnico extends Pessoa {
 	
+	
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Tecnico() {
 		super();
+		addPerfis(Perfil.CLIENTE);
 		// TODO Auto-generated constructor stub
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfis(Perfil.CLIENTE);
 		// TODO Auto-generated constructor stub
 	}
 
