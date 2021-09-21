c4ad0bbc7202f3e09b534f103726c4934e11b673
Jose Carlos Maria Junior <jotacfs2010@hotmail.com>
2021-09-20 23:10:33 -0300

    proketo iniciado

---
+++ src/main/java/com/jcmj/SistemaBaseApplication.java
@@ -0,0 +1,13 @@
+package com.jcmj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jcmj/SistemaBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcmj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
