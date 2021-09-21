409f6fee0b3417e0c59d5a90813de6e772024643
Jose Carlos Maria Junior <jotacfs2010@hotmail.com>
2021-09-21 01:29:10 -0300

    Iniciando as classes de dominio

---
+++ src/main/java/com/jcmj/domain/Pessoa.java
@@ -0,0 +1,157 @@
+package com.jcmj.domain;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.jcmj.domain.enus.Perfil;
+
+
+
+public abstract class Pessoa {
+	
+	
+	protected Integer id;
+	protected String nome;
+	protected String cpf;
+	protected String email;
+	protected String senha;	
+	protected Set<Integer>perfis = new HashSet<>();
+	protected LocalDate dataCricacao = LocalDate.now();
+	
+	public Pessoa() {
+		super();
+		addPerfis(Perfil.CLIENTE);
+	}
+
+	public Pessoa(Integer id, String nome, String cpf, String email, String senha) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+		this.senha = senha;
+		addPerfis(Perfil.CLIENTE);
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Set<Perfil> getPerfis() {
+		return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+	}
+
+	public void addPerfis(Perfil perfil) {
+		this.perfis.add(perfil.getCodigo());
+	}
+
+	public LocalDate getDataCricacao() {
+		return dataCricacao;
+	}
+
+	public void setDataCricacao(LocalDate dataCricacao) {
+		this.dataCricacao = dataCricacao;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+		result = prime * result + ((dataCricacao == null) ? 0 : dataCricacao.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((perfis == null) ? 0 : perfis.hashCode());
+		result = prime * result + ((senha == null) ? 0 : senha.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		if (cpf == null) {
+			if (other.cpf != null)
+				return false;
+		} else if (!cpf.equals(other.cpf))
+			return false;
+		if (dataCricacao == null) {
+			if (other.dataCricacao != null)
+				return false;
+		} else if (!dataCricacao.equals(other.dataCricacao))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (perfis == null) {
+			if (other.perfis != null)
+				return false;
+		} else if (!perfis.equals(other.perfis))
+			return false;
+		if (senha == null) {
+			if (other.senha != null)
+				return false;
+		} else if (!senha.equals(other.senha))
+			return false;
+		return true;
+	}
+	
+	
+
+}
